c7d58324ec3cfdd8c1035d562bb756fdccec8570
Nicolle <nifalbo@uol.com.br>
2019-09-19 11:08:42 -0400

    Add files via upload

---
+++ yahtzee/Test.h
@@ -0,0 +1,37 @@
+//**********************************************************************************//
+// Nom : Test.h                                                                     //
+//                                                                                  //
+// Auteur : Marcel & Nicolle                                                        //
+//                                                                                  //
+// Date : 12 octobre 2018                                                           //
+//                                                                                  //
+//**********************************************************************************//
+
+#pragma once
+#include "Yahtzee.h"
+
+//**********************************************************************************//
+// tests                                                                            //
+//                                                                                  //
+// Quand l'utilisateur entre les mots "test" ou "TEST" las fonctions de test sont   //
+// activées.     																	//
+//                                                                                  //
+//**********************************************************************************//
+
+void testerTout();
+
+void testBrelan();
+
+void testCarre();
+
+void testMainPleine();
+
+void testPetiteSuite();
+
+void testGrandeSuite();
+
+void testYahtzee();
+
+void testChance();
+
+
